8171947dbd018be9aa1f0368ff457e5b2cd28301
SimoneFerrini <ferrini.simone4@gmail.com>
2023-06-13 13:02:21 +0200

    feat: add house migration

---
+++ database/migrations/2023_06_13_104127_create_houses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 100)->required();
+            $table->text('description', 500)->required();
+            $table->tinyInteger('rooms')->required();
+            $table->smallInteger('beds')->required();
+            $table->tinyInteger('bathrooms')->required();
+            $table->smallInteger('square_mt')->required();
+            $table->string('street')->required();
+            $table->string('city')->required();
+            $table->smallInteger('house_number')->required();
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->text('thumbnail')->required();
+            $table->boolean('visibility');
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+};
