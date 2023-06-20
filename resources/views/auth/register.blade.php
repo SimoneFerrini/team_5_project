e4cc44043b3013f466aabc622abe0ca5d493633d
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-06-20 09:22:04 +0200

    feat: confirm password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -71,7 +71,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} *</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup='check();'>
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -85,7 +85,8 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }} *</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" onkeyup='check();'>
+                                <span id='message'></span>
                             </div>
                         </div>
                         <span>* I campi sono obbligatori!</span>
@@ -102,4 +103,17 @@
         </div>
     </div>
 </div>
+
+<script>
+var check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('password-confirm').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'La password corrisponde!';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'La password non corrisponde!';
+    }
+  }
+  </script>
 @endsection
