8d6291d32dfc4716a4e68bcd9fca17851cedaeed
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-06-21 15:51:24 +0200

    feat: add service to api

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -12,31 +12,31 @@ class HouseController extends Controller
 
 
     //ricordati di aggiungere le altre tabelle collegate ad es, messaggi, statistiche e premium nel with
-    public function index() {
+    public function index()
+    {
 
-        $houses = House::all();
+        $houses = House::with('services');
 
         return response()->json([
             'success' => true,
             'results' => $houses
         ]);
-
     }
 
-   // public function show($id){
-   //     $house = House::where('id', $id)->with('services')->first();
-//
-   //     if($house){
-   //         return response()->json([
-   //             'success' => true,
-   //             'house'=> $house,
-   //         ]);          
-   //     } else {
-   //         return response()->json([
-   //             'success' => false,
-   //             'error'=> 'House not found'
-   //         ]);
-   //     }
-   // }
+    // public function show($id){
+    //     $house = House::where('id', $id)->with('services')->first();
+    //
+    //     if($house){
+    //         return response()->json([
+    //             'success' => true,
+    //             'house'=> $house,
+    //         ]);          
+    //     } else {
+    //         return response()->json([
+    //             'success' => false,
+    //             'error'=> 'House not found'
+    //         ]);
+    //     }
+    // }
 
 }
