ee7cd36d9b0b6130412bb564dde23bbbd2cb6c9a
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-06-23 13:36:04 +0200

    feat: filter options

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\House;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HouseController extends Controller
@@ -12,31 +13,46 @@ class HouseController extends Controller
 
 
     //ricordati di aggiungere le altre tabelle collegate ad es, messaggi, statistiche e premium nel with
-    public function index()
+    public function index(Request $request)
     {
+        $filtri = $request->all();
 
-        $houses = House::with('services')->get();
 
-        return response()->json([
-            'success' => true,
-            'results' => $houses
-        ]);
+        if ($request->has('filtri') && $filtri) {
+            $services_ids = explode(',', $filtri['filtri']);
+
+            $houses = House::whereHas('services', function ($query) use ($services_ids) {
+                $query->whereIn('service_id', $services_ids);
+            }, '=', count($services_ids))
+                ->with('services')->get();
+
+            return response()->json([
+                'success' => true,
+                'results' => $houses
+            ]);
+        } else {
+            $results = House::with('services')->get();
+
+            return response()->json([
+                'success' => true,
+                'results' => $results
+            ]);
+        }
     }
+    public function show($id)
+    {
+        $house = House::where('id', $id)->with('services')->first();
 
-     public function show($id){
-         $house = House::where('id', $id)->with('services')->first();
-    
-         if($house){
-             return response()->json([
-                 'success' => true,
-                 'house'=> $house,
-             ]);          
-         } else {
-             return response()->json([
-                 'success' => false,
-                 'error'=> 'House not found'
-             ]);
-         }
-     }
-
+        if ($house) {
+            return response()->json([
+                'success' => true,
+                'house' => $house,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'House not found'
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\HouseController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('houses', [HouseController::class, 'index']);
+
+Route::get('services', [ServiceController::class, 'index']);
+
 
 Route::get('houses/{id}', [HouseController::class, 'show']);
