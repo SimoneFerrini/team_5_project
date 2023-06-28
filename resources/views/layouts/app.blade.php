46fa0a92f1cca8ea8ce4f781ed305879d0ef67f8
GiuliaSetti <giulia.setti96@gmail.com>
2023-06-28 16:44:53 +0200

    feat: footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,32 @@
         <main class="">
             @yield('content')
         </main>
+
+
+        <footer id="footer_back">
+            <div id="footer">
+                <!-- sinistra -->
+                <div class="links">
+                    <ul class="pt-4">
+                    <li><a href="#">Lavora con noi</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contatti</a></li>
+                    <li><a href="#">Termini&Servizi</a></li>
+                    </ul>
+                </div>
+                <!-- /sinistra -->
+                
+                <!-- destra -->
+
+                <div class="footer_right">
+                    <img class="long_logo" src="{{asset('images/logochiaro.png')}}" alt="logo">
+                    <img class="short_logo" src="{{asset('images/logo-bool.png')}}" alt="logo">
+
+                </div>
+                <!-- /destra -->
+            </div>
+             
+        </footer>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
