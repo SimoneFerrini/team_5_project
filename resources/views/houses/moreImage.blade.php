9411e0fca0f523e8ac981331b6c251c38aa75132
GiuliaSetti <giulia.setti96@gmail.com>
2023-06-28 23:00:48 +0200

    feat: styled moreImage blade

--- resources/views/houses/moreImage.blade.php
+++ resources/views/houses/moreImage.blade.php
@@ -3,11 +3,11 @@
 @if($house->user_id != $logged_user_id)
   <span>Non puoi visualizzare questa casa!</span>
 @else
-<div class="container">
+<div class="container my-5 w-100 mx-auto text-center">
   <h1>Casa aggiunta con successo!</h1>
-  <h3>Vuoi aggiungere altre immagini?</h3>
-  <button class="btn btn-secondary"><a href="{{route('image.create', $house)}}" class="link-light">Si</a></button>
-  <button class="btn btn-secondary"><a href="{{route('houses.show', $house)}}" class="link-light">No</a></button>
+  <h3 class="my-5">Vuoi aggiungere altre immagini?</h3>
+  <a href="{{route('image.create', $house)}}"><button class="orange_btn">Si</button></a>
+  <a href="{{route('houses.show', $house)}}"><button class="green_btn">No</button></a>
 
 
 </div>
