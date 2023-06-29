f07471c22b74cda241bc42aed1da172b5be843f6
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-06-29 09:51:47 +0200

    Fix: edit image

--- resources/views/houses/uploadImage.blade.php
+++ resources/views/houses/uploadImage.blade.php
@@ -1,48 +1,25 @@
 @extends('layouts.app')
 @section('content')
 @if($house->user_id != $logged_user_id)
-  <span>Non puoi visualizzare questa casa!</span>
+<span>Non puoi visualizzare questa casa!</span>
 @else
 <div class="upload_image">
-
+  
   <div class="my-thumb">
     <img src="{{asset('storage/'. $house->thumbnail)}}" alt="thumb">
-
+    
   </div>
 
   <div class="house_images">
-      @foreach ($images as $singleImage)
+    @foreach ($images as $singleImage)
       <div class="single_image">
         <img src="{{asset('/'.$singleImage->path)}}" alt="img">
 
         <div class="house_images_btn">
               <button type="submit" data-toggle="modal" data-target="#modalMessageDelete">
                   Elimina foto
-              </button>       
+                </button>       
         </div>
-
-      </div>
-      
-      @endforeach
-
-      <div class="w-100 container mx-auto my-5">
-        <form action="{{ route('images.store', $house->id) }}" method="POST" enctype="multipart/form-data">
-          @csrf
-          <h1 class="text-center">Carica altre immagini</h1>
-          <hr>
-          <div>
-            <input type="file" name="images[]" multiple class="choose_img">
-          </div>
-  
-          <hr>
-  
-          <div class="upload_images_btns">
-            <button type="submit">Invia</button>
-            <a href="{{route('houses.show', $house)}}"><button class="annulla">Annulla</button></a>
-  
-          </div>
-  
-        </form>
 
       </div>
       <div class="modal fade" id="modalMessageDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -69,8 +46,30 @@
                   </div>
                 </div>
               </div>
-
             </div>
+      
+      @endforeach
+      
+      <div class="w-100 container mx-auto my-5">
+        <form action="{{ route('images.store', $house->id) }}" method="POST" enctype="multipart/form-data">
+          @csrf
+          <h1 class="text-center">Carica altre immagini</h1>
+          <hr>
+          <div>
+            <input type="file" name="images[]" multiple class="choose_img">
+          </div>
+          
+          <hr>
+          
+          <div class="upload_images_btns">
+            <button type="submit">Invia</button>
+            <a href="{{route('houses.show', $house)}}"><button class="annulla">Annulla</button></a>
+            
+          </div>
+          
+        </form>
+
+      </div>
   
       </div>
 
